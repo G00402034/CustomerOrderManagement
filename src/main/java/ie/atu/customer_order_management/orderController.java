4b21701315821c2c8f482362fba76abc90c4c8fb
G00402034 <G00402034@atu.ie>
2023-11-08 14:47:30 +0000

    added orderController to handle HTTP requests

--- src/main/java/ie/atu/customer_order_management/orderController.java
+++ src/main/java/ie/atu/customer_order_management/orderController.java
@@ -1,4 +1,29 @@
 package ie.atu.customer_order_management;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
 public class orderController {
+
+    private orderService orderService;
+    private orderDetails orderDetails;
+
+    @Autowired
+    public void Inject(orderService(orderService)){this.orderService = orderService};
+
+    @PostMapping("/addOrder")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addOrderReciever(@RequestBody orderDetails orderDetails){orderService.createOrder(orderDetails);}
+
+    @PostMapping("/getorder")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ArrayList <orderDetails> getOrderReciever(){return orderService.getOrderByid();}
+
 }
--- src/main/java/ie/atu/customer_order_management/orderService.java
+++ src/main/java/ie/atu/customer_order_management/orderService.java
@@ -3,7 +3,7 @@ package ie.atu.customer_order_management;
 import java.util.ArrayList;
 
 public class orderService {
-    ArrayList<orderDetails> ordersSheet = new ArrayList<~>();
-    public createOrder (orderDetails orderDetails){ordersSheet.add(orderDetails);}
+    ArrayList<orderDetails> ordersSheet = new ArrayList<>();
+    public void createOrder (orderDetails orderDetails){ordersSheet.add(orderDetails);}
     public void Arraylist <orderDetails> getOrderByid(){return ordersSheet;}
 }
